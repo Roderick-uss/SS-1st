003135b706f8a0ad6d4e768c6352f9c39ec9eeda
Sergei <ushakov.sa@phystech.edu>
2024-09-26 18:38:08 +0300

    wrong repos

--- Onegin2/sources/colors.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLOR_H
-#define COLOR_H
-
-#define GREEN  "\033[32m"
-#define RED    "\033[31m"
-#define WHITE  "\033[37m"
-#define YELLOW "\033[33m"
-
-#endif // COLOR_H
--- Onegin2/sources/sorter.h
+++
@@ -1,9 +0,0 @@
-#ifndef  SORTER
-#define  SORTER
-
-typedef int (*cmp_t)(const void*, const void*);
-
-void bubble_sort(void* base, size_t size, size_t el_size, cmp_t cmp);
-void quic_sort  (void* base, size_t size, size_t el_size, cmp_t cmp);
-
-#endif// SORTER
